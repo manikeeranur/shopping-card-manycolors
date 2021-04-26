3a4dc53987bcf28d8e9942036921127e372d295f
manikeeranur <63514297+manikeeranur@users.noreply.github.com>
2021-04-27 00:09:55 +0530

    card-6

--- js/script.js
+++ js/script.js
@@ -1,44 +1,131 @@
 $(".black").click(function () {
-  $("img").attr("src", "./images/watches_black.png");
-  $(".black").addClass("box-border");
+  $(this).addClass("box-border");
   $(".blue").removeClass("box-border");
   $(".green").removeClass("box-border");
   $(".red").removeClass("box-border");
   $(".yellow").removeClass("box-border");
 });
-
 $(".blue").click(function () {
-  $("img").attr("src", "./images/watches_blue.png");
   $(".black").removeClass("box-border");
-  $(".blue").addClass("box-border");
+  $(this).addClass("box-border");
   $(".green").removeClass("box-border");
   $(".red").removeClass("box-border");
   $(".yellow").removeClass("box-border");
 });
-
 $(".green").click(function () {
-  $("img").attr("src", "./images/watches_green.png");
   $(".black").removeClass("box-border");
   $(".blue").removeClass("box-border");
-  $(".green").addClass("box-border");
+  $(this).addClass("box-border");
   $(".red").removeClass("box-border");
   $(".yellow").removeClass("box-border");
 });
-
 $(".red").click(function () {
-  $("img").attr("src", "./images/watches_red.png");
   $(".black").removeClass("box-border");
   $(".blue").removeClass("box-border");
   $(".green").removeClass("box-border");
-  $(".red").addClass("box-border");
+  $(this).addClass("box-border");
   $(".yellow").removeClass("box-border");
 });
-
 $(".yellow").click(function () {
-  $("img").attr("src", "./images/watches_yellow.png");
   $(".black").removeClass("box-border");
   $(".blue").removeClass("box-border");
   $(".green").removeClass("box-border");
   $(".red").removeClass("box-border");
-  $(".yellow").addClass("box-border");
+  $(this).addClass("box-border");
+});
+
+$("#polo_1").click(function () {
+  $("img.polo").attr("src", "./images/polo_shirt/polo_shirt_black.png");
+});
+$("#polo_2").click(function () {
+  $("img.polo").attr("src", "./images/polo_shirt/polo_shirt_blue.png");
+});
+$("#polo_3").click(function () {
+  $("img.polo").attr("src", "./images/polo_shirt/polo_shirt_green.png");
+});
+$("#polo_4").click(function () {
+  $("img.polo").attr("src", "./images/polo_shirt/polo_shirt_red.png");
+});
+$("#polo_5").click(function () {
+  $("img.polo").attr("src", "./images/polo_shirt/polo_shirt_yellow.png");
+});
+
+$("#shirt001_black").click(function () {
+  $("img.shirt001").attr("src", "./images/shirt001/shirt001_black.png");
+});
+$("#shirt001_blue").click(function () {
+  $("img.shirt001").attr("src", "./images/shirt001/shirt001_blue.png");
+});
+$("#shirt001_green").click(function () {
+  $("img.shirt001").attr("src", "./images/shirt001/shirt001_green.png");
+});
+$("#shirt001_red").click(function () {
+  $("img.shirt001").attr("src", "./images/shirt001/shirt001_red.png");
+});
+$("#shirt001_yellow").click(function () {
+  $("img.shirt001").attr("src", "./images/shirt001/shirt001_yellow.png");
+});
+
+$("#shirt002_black").click(function () {
+  $("img.shirt002").attr("src", "./images/shirt002/shirt002_black.png");
+});
+$("#shirt002_blue").click(function () {
+  $("img.shirt002").attr("src", "./images/shirt002/shirt002_blue.png");
+});
+$("#shirt002_green").click(function () {
+  $("img.shirt002").attr("src", "./images/shirt002/shirt002_green.png");
+});
+$("#shirt002_red").click(function () {
+  $("img.shirt002").attr("src", "./images/shirt002/shirt002_red.png");
+});
+$("#shirt002_yellow").click(function () {
+  $("img.shirt002").attr("src", "./images/shirt002/shirt002_yellow.png");
+});
+
+$("#tshirt001_black").click(function () {
+  $("img.tshirt001").attr("src", "./images/tshirt001/tshirt001_black.png");
+});
+$("#tshirt001_blue").click(function () {
+  $("img.tshirt001").attr("src", "./images/tshirt001/tshirt001_blue.png");
+});
+$("#tshirt001_green").click(function () {
+  $("img.tshirt001").attr("src", "./images/tshirt001/tshirt001_green.png");
+});
+$("#tshirt001_red").click(function () {
+  $("img.tshirt001").attr("src", "./images/tshirt001/tshirt001_red.png");
+});
+$("#tshirt001_yellow").click(function () {
+  $("img.tshirt001").attr("src", "./images/tshirt001/tshirt001_yellow.png");
+});
+
+$("#tshirt002_black").click(function () {
+  $("img.tshirt002").attr("src", "./images/tshirt002/tshirt002_black.png");
+});
+$("#tshirt002_blue").click(function () {
+  $("img.tshirt002").attr("src", "./images/tshirt002/tshirt002_blue.png");
+});
+$("#tshirt002_green").click(function () {
+  $("img.tshirt002").attr("src", "./images/tshirt002/tshirt002_green.png");
+});
+$("#tshirt002_red").click(function () {
+  $("img.tshirt002").attr("src", "./images/tshirt002/tshirt002_red.png");
+});
+$("#tshirt002_yellow").click(function () {
+  $("img.tshirt002").attr("src", "./images/tshirt002/tshirt002_yellow.png");
+});
+
+$("#watches_black").click(function () {
+  $("img.watches").attr("src", "./images/watches/watches_black.png");
+});
+$("#watches_blue").click(function () {
+  $("img.watches").attr("src", "./images/watches/watches_blue.png");
+});
+$("#watches_green").click(function () {
+  $("img.watches").attr("src", "./images/watches/watches_green.png");
+});
+$("#watches_red").click(function () {
+  $("img.watches").attr("src", "./images/watches/watches_red.png");
+});
+$("#watches_yellow").click(function () {
+  $("img.watches").attr("src", "./images/watches/watches_yellow.png");
 });
